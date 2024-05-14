0c70821197cdf7979f159749729058cd59619761
Tanmay Gadge <tanmaygadge9273@gmail.com>
2024-05-14 17:42:18 +0530

    parallax scroll and scroll reveal

---
+++ scroll-reveal/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    ScrollReveal().reveal(".reveal", {
+        origin: 'bottom',
+        distance: '50px',
+        duration: 1000,
+        delay: 200,
+        reset: true
+    });
+})
